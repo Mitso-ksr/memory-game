c15e5a9235f6a25057e1ad58745ce7b58c4827fe
mitso-ksr <mitsonary@gmail.com>
2024-01-31 19:32:08 +0330

    the first commit

---
+++ script.js
@@ -0,0 +1,4 @@
+/* the handle function should add the class show to the
+ card-font and remove class hide from the card. and also use the data- attribute
+ to check if the selected cards refer to the same picture.
+ */
